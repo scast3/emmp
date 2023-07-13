c4e3cb86656165c6b8ec815448aa7e705d45d6c7
scast_work <scastillo@englewoodco.gov>
2023-07-13 16:02:18 -0600

    HTML now works, will display appropriate changes, just need to edit the js file

--- pumpcalc.js
+++ pumpcalc.js
@@ -1,4 +1,4 @@
-function power(Q1,w1,h1,n1,Q2,w2,h2,n2){
+function power(flow1,speed1,head1,efficiency1,flow2,speed2,head2,efficiency2){
     const density = 62.4; // Density of water in lb/ft³
     const g = 32.2; // Acceleration due to gravity in ft/s²
   
@@ -9,7 +9,7 @@ function power(Q1,w1,h1,n1,Q2,w2,h2,n2){
     const power2 = (flow2 * head2 * density * g) / (550 * efficiency2 * Math.pow(speed2, 3));
   
     // Calculate the difference in power
-    const powerDifference = Math.abs(power1 - power2);
+    const powerDifference = power2 - power1;
   
     return powerDifference;
 }

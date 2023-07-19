ffcd11ecd16a45c435943afcc9fb12bfed40f6e8
scast_work <scastillo@englewoodco.gov>
2023-07-19 07:26:35 -0600

    Displays old and new pump power, still need to add cost calcs

--- pumpcalc.js
+++ pumpcalc.js
@@ -1,18 +1,25 @@
-function power(flow1,speed1,head1,efficiency1,flow2,speed2,head2,efficiency2){
-    const density = 62.4; // Density of water in lb/ft³
-    const g = 32.2; // Acceleration due to gravity in ft/s²
-  
+
+function power(q, h, n) {
+    const density = 62.4;
+    const g = 32.2;
+    const power = (q * h * density * g) / (550 * n);
+    console.log(power);
+    return power;
+}
+
+function powerdif(flow1, head1, efficiency1, flow2, head2, efficiency2) {
     // Calculate power for Pump 1
-    const power1 = (flow1 * head1 * density * g) / (550 * efficiency1);
-  
+    const power1 = power(flow1, head1, efficiency1);
+
     // Calculate power for Pump 2
-    const power2 = (flow2 * head2 * density * g) / (550 * efficiency2);
-  
+    const power2 = power(flow2, head2, efficiency2);
+
     // Calculate the difference in power
     const powerDifference = power2 - power1;
-  
+
     return powerDifference;
 }
-function costs(){
+
+function costs(power1,power2){
     return 1;
 }
